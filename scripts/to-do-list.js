bc8ecb52607743217bc0602df69bd921c3390bc6
lucicek <mo.vitek@seznam.cz>
2025-10-01 14:58:34 +0200

    Add files via upload

---
+++ scripts/to-do-list.js
@@ -0,0 +1,51 @@
+const tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+
+const toDoList = [
+  {
+    name: '<div style="margin-right: 4px;">Navštívit</div> <a href="index.html"> vitek.soon.it</a>',
+    dueDate: tomorrow.toLocaleDateString('cs-CZ')
+  }
+];
+
+renderToDoList();
+
+function renderToDoList(){
+  let toDoListHTML = ''
+
+  toDoList.forEach((toDoObject, idex)=>{
+    const { name, dueDate } = toDoObject;
+    const html = `
+      <div class="info">${name}</div>
+      <div class="info">${dueDate}</div>
+      <button class="js-remove-to-do-object-button remove-button">Odstranit</button>`;
+    toDoListHTML += html;
+  });
+
+  document.querySelector('.js-to-do-list').innerHTML = toDoListHTML;
+
+  document.querySelectorAll('.js-remove-to-do-object-button').forEach((removeButton, index)=>{
+    removeButton.addEventListener('click', ()=>{
+      toDoList.splice(index, 1);
+      renderToDoList();
+    })
+  })
+};
+
+document.querySelector('.js-to-do-list-add-button').addEventListener('click', ()=>{
+  addToDo();
+});
+
+function addToDo() {
+  const name = document.querySelector('.js-name-input').value;
+
+  const dueDateInput = document.querySelector('.js-due-date-input').value;
+  const dueDate = new Date(dueDateInput).toLocaleDateString('cs-CZ');
+
+  toDoList.push({
+    name,
+    dueDate
+  });
+
+  renderToDoList();
+};
